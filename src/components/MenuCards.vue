675c32bb717f02d6632cff9fe693b074d21b0e32
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-25 16:43:57 +0300

    Create MenuCards.vue

---
+++ src/components/MenuCards.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+
+        <MenuCard v-for="item in menu" :key="item.id" :menuItem="item" />
+
+    </div>
+</template>
+
+<script>
+import menu from '../assets/menu.json';
+import MenuCard from '../components/MenuCard.vue'
+export default {
+    name: 'MenuCards',
+    components: {
+
+        MenuCard
+    },
+    data() {
+        return {
+            menu: menu
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
