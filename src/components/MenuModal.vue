16a28ade2cde626af8d3690f5971bb03f7072a09
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-25 17:10:23 +0300

    Update MenuModal.vue

--- src/components/MenuModal.vue
+++ src/components/MenuModal.vue
@@ -1,1 +1,27 @@
-<!--  -->
+<template>
+    <div class="text-center">
+        <v-dialog v-model="dialog">
+
+
+            <v-card>
+                <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dialog: false,
+        }
+    },
+}
+</script>
