9d29c1be5ec53b0ef1f28f289a2c6a0e4a69dbbd
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-25 16:43:55 +0300

    Create MenuCard.vue

---
+++ src/components/MenuCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col  s12 m3 ">
+        <div class="card ">
+            <div class="card-image">
+                <img :key="menuItem.id" :src=menuItem.img>
+            </div>
+            <div class="card-content ">
+                <span class="card-title" :key="menuItem.id">{{ menuItem.title }}</span>
+                <p class="truncate" :key="menuItem.id">
+                    {{ menuItem.detail }}</p>
+                <a class="btn-floating right halfway-fab waves-effect waves-light  green"><i
+                        class="material-icons">+</i></a>
+                <!-- <a class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons">+</i></a> -->
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuCard',
+    props: {
+        menuItem: Object,
+    },
+
+}
+</script>
+
+<style >
+/* .card-item-container {
+    height: 300px;
+} */
+</style>
