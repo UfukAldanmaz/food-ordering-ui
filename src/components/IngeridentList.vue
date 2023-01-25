077cba85115105bd77fb0987b8029aae5ba13c5a
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-25 23:40:54 +0300

    Create IngeridentList.vue

---
+++ src/components/IngeridentList.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <v-table>
+        <tbody>
+            <tr>
+                <td><img class="submenu-img" :key="ingeridents.id" :src=ingeridents.img /> </td>
+                <td>{{ ingeridents.name }}</td>
+                <td>{{ ingeridents.price }} ₽</td>
+                <td><input type="checkbox" />
+                    <CounterItem />
+                </td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<script>
+import CounterItem from './CounterItem.vue';
+
+export default {
+    name: 'IngeridentList',
+    components: {
+        CounterItem
+    },
+    props: {
+        ingeridents: Object,
+    },
+}
+
+</script>
+
+<style>
+.submenu-img {
+    width: 50px
+}
+</style>
