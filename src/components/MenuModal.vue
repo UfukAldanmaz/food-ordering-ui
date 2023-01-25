709286b3e6c2cd261745337cafd6f998251a847e
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-25 23:41:00 +0300

    Update MenuModal.vue

--- src/components/MenuModal.vue
+++ src/components/MenuModal.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="text-center">
-        <v-dialog v-model="dialog">
-
+        <v-dialog width="350px" v-model="dialog">
+            <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props">
+                    Open Dialog
+                </v-btn>
+            </template>
 
             <v-card>
+                <img :key="menuItem.id" :src=menuItem.img>
+
                 <v-card-text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua.
+                    <h5 class="card-title" :key="menuItem.id">{{ menuItem.title }}</h5>
+                    <p class="truncate" :key="menuItem.id">
+                        {{ menuItem.detail }}</p>
+
+                    <IngeridentList v-for="item in ingeridents" :key="item.id" :ingeridents="item" />
+
+
                 </v-card-text>
                 <v-card-actions>
                     <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
@@ -17,7 +28,16 @@
 </template>
 
 <script>
+import IngeridentList from './IngeridentList.vue';
 export default {
+    name: 'MenuModal',
+    components: {
+        IngeridentList,
+    },
+    props: {
+        menuItem: Object,
+        ingeridents: Object,
+    },
     data() {
         return {
             dialog: false,
