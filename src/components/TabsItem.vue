cf80740427c3ef7df7330eeba7753a5592cca1c1
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-26 15:49:48 +0300

    Create TabsItem.vue

---
+++ src/components/TabsItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tab-container">
+        <v-tabs fixed-tabs background-color="white" dark>
+            <v-tab>
+                Option
+            </v-tab>
+            <v-tab>
+                Another Selection
+            </v-tab>
+            <v-tab>
+                Items
+            </v-tab>
+            <v-tab>
+                Another Screen
+            </v-tab>
+        </v-tabs>
+    </div>
+</template>
+
+<style>
+.tab-container {
+    margin-bottom: 50px;
+}
+</style>
