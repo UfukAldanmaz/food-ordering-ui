5ad6f50305aef4d4e38e5874ef8ce8ede3607b9f
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-26 15:49:46 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
-import HomepageItem from './views/HomepageItem.vue'
-import AboutItem from './views/AboutItem.vue'
-import { createRouter, createWebHistory } from "vue-router"
-const routeInfos = [
-    {
-        path: "/",
-        component: HomepageItem
-    },
-    {
-        path: "/about",
-        component: AboutItem
-    }
-]
+// import HomepageItem from './views/HomepageItem.vue'
+// import AboutItem from './views/AboutItem.vue'
+// import { createRouter, createWebHistory } from "vue-router"
+// const routeInfos = [
+//     {
+//         path: "/",
+//         component: HomepageItem
+//     },
+//     {
+//         path: "/about",
+//         component: AboutItem
+//     }
+// ]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: routeInfos
-})
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes: routeInfos
+// })
 
-export default router;
+// export default router;
