b289c5ca6d28cfb45ec30fa139ea93113a383061
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-26 15:49:37 +0300

    Create CartItem.vue

---
+++ src/components/CartItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cart">
+    Cart
+  </div>
+</template>
+<style>
+.cart {
+  /* width: 200px; */
+  border: 1px solid black;
+  height: 500px;
+  background-color: gray;
+  float: right;
+  width: 20%;
+}
+</style>
