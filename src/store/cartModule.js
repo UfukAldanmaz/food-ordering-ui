5e587810c2349d7cec7ee836e5641708c75a12be
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 22:26:24 +0300

    Update cartModule.js

--- src/store/cartModule.js
+++ src/store/cartModule.js
@@ -14,12 +14,27 @@ export default {
             state.orders = payload
         },
         INCREASE_ORDER(state, payload) {
-            state.orders = payload
-        }
-
+            state.orders = state.orders.map(order => {
+                if (order.orderId === payload.orderId) {
+                    console.log("xxxxxxxx", order, payload)
+                    order.productCount += 1;
+                }
+                return order;
+            });
+        },
+        DECREASE_ORDER(state, payload) {
+            state.orders = state.orders.map(order => {
+                if (order.orderId === payload.orderId) {
+                    console.log("oooooo", order)
+                    order.productCount -= 1;
+                }
+                return order;
+            });
+        },
     },
     actions: {
         addToCart(context, payload) {
+            console.log("ppppp", payload)
             const orders = context.state.orders
             orders.push(payload)
             context.commit('ADD_TO_CART', orders)
@@ -32,8 +47,13 @@ export default {
                 order => order !== payload
             )
             context.commit('REMOVE_ORDER', orders)
-        }
-
+        },
+        increaseOrder(context, payload) {
+            context.commit('INCREASE_ORDER', payload)
+        },
+        decreaseOrder(context, payload) {
+            context.commit('DECREASE_ORDER', payload)
+        },
     },
     getters: {
         totalPrice: function ({ orders }) {
@@ -44,15 +64,25 @@ export default {
                 result += order.product.price * order.productCount;
                 for (let j = 0; j < order.ingeridents.length; j++) {
                     const ingerident = order.ingeridents[j];
-                    result += ingerident.ingerident.price * ingerident.count;
+                    result += ingerident.ingerident.price * ingerident.count * order.productCount;
                 }
             }
 
             return result;
         },
         getOrders: function ({ orders }) {
-            console.log("orders", orders);
             return orders;
+        },
+        getMaxOrderId: function ({ orders }) {
+            let max = 0;
+            for (let i = 0; i < orders.length; i++) {
+                const order = orders[i];
+                if (max < order.orderId) {
+                    max = order.orderId;
+                }
+            }
+
+            return max;
         }
     }
 }
