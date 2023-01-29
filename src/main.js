e579e632196d6f36d0f85f21bf31df4a6b50c77a
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 22:34:56 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import router from './router'
 import { loadFonts } from './plugins/webfontloader'
 import store from './store'
 
@@ -9,6 +8,5 @@ loadFonts()
 
 createApp(App)
     .use(vuetify)
-    .use(router)
     .use(store)
     .mount('#app')
