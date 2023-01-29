a13ef2e72f29b5c9286d47017abb794cb397244e
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 12:48:07 +0300

    Update IngeridentList.vue

--- src/components/IngeridentList.vue
+++ src/components/IngeridentList.vue
@@ -1,36 +1,56 @@
 <template>
-
+    <h3>{{ ingeridentCategory?.name }}</h3>
     <v-table>
         <tbody>
-            <tr>
-                <td><img class="submenu-img" :key="ingeridents.id" :src=ingeridents.img /> </td>
-                <td>{{ ingeridents.name }}</td>
-                <td>{{ ingeridents.price }} ₽</td>
-                <td><input type="checkbox" />
-                    <CounterItem />
-                </td>
+            <tr v-for="ingerident in ingeridents" :key="ingerident.id">
+                <IngeridentItem :ingerident="ingerident" :key="ingerident.id" @ingeridentRemoved="ingeridentRemoved"
+                    @ingeridentAdded="ingeridentAdded" />
             </tr>
         </tbody>
     </v-table>
 </template>
 
 <script>
-import CounterItem from './CounterItem.vue';
+import IngeridentItem from './IngeridentItem.vue'
+import * as ingeridentCategoryService from '../services/ingeridentCategoryService'
 
 export default {
     name: 'IngeridentList',
     components: {
-        CounterItem
+        IngeridentItem,
     },
     props: {
         ingeridents: Object,
     },
+    data() {
+        return {
+            ingeridentCategory: null,
+            selectedIngeridents: []
+        }
+    },
+    mounted() {
+        const firstIngerident = this.ingeridents && this.ingeridents.length ? this.ingeridents[0] : undefined
+
+        if (!firstIngerident) {
+            return
+        }
+
+        this.ingeridentCategory = ingeridentCategoryService.get(firstIngerident.categoryId);
+    },
+    methods: {
+        ingeridentAdded(ingerident, count) {
+            this.selectedIngeridents.push({
+                ingerident,
+                count
+            });
+
+            this.$emit("ingeridentsChanged", this.selectedIngeridents);
+        },
+        ingeridentRemoved(ingerident) {
+            this.selectedIngeridents = this.selectedIngeridents.filter(x => x.id !== ingerident.id);
+            this.$emit("ingeridentsChanged", this.selectedIngeridents);
+        }
+    }
 }
 
 </script>
-
-<style>
-.submenu-img {
-    width: 50px
-}
-</style>
