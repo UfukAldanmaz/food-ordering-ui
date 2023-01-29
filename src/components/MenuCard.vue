1c230a9a7895aceb1738c991ba66e34992ffc158
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 22:26:13 +0300

    Update MenuCard.vue

--- src/components/MenuCard.vue
+++ src/components/MenuCard.vue
@@ -10,8 +10,10 @@
                 <span class="card-title" :key="menuItem.id">{{ menuItem.title }}</span>
                 <p class="truncate" :key="menuItem.id">
                     {{ menuItem.detail }}</p>
-
-                <MenuModal :menuItem="menuItem" />
+                <div class="modal-con">
+                    <span class="menu-price">{{ menuItem.price }} ₽</span>
+                    <MenuModal :menuItem="menuItem" />
+                </div>
 
                 <!-- <a class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons">+</i></a> -->
                 <!-- // v-model="showMenuModal" -->
@@ -40,6 +42,19 @@ export default {
 </script>
 
 <style  >
+.modal-con {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 5px;
+    align-items: center;
+}
+
+.menu-price {
+    font-weight: bolder;
+    font-size: 20px;
+}
+
 /* .menu-detail-text {
     overflow: hidden;
     display: -webkit-box;
