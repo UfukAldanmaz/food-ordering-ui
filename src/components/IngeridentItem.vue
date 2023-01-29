f19e767fa1a5d89fe9de443ab01e05c5cd73aa61
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 12:48:02 +0300

    Create IngeridentItem.vue

---
+++ src/components/IngeridentItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <td><img class="submenu-img" :key="ingerident.id" :src=ingerident.img /> </td>
+    <td>{{ ingerident.name }}</td>
+    <td>{{ ingerident.price }} ₽</td>
+    <td>
+        <CounterItem @onCounterChanged="ingeridentCounterChanged" />
+    </td>
+</template>
+
+<script>
+import CounterItem from './CounterItem.vue';
+
+export default {
+    name: "IngeridentItem",
+    components: {
+        CounterItem
+    },
+    props: {
+        ingerident: Object,
+    },
+    data() {
+        return {
+            ingeridentCount: 0,
+        }
+    },
+    methods: {
+        ingeridentCounterChanged(count) {
+            this.ingeridentCount = count;
+            if (this.ingeridentCount === 0) {
+                this.$emit("ingeridentRemoved", this.ingerident);
+            } else {
+                this.$emit("ingeridentAdded", this.ingerident, this.ingeridentCount);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.submenu-img {
+    width: 50px
+}
+</style>
