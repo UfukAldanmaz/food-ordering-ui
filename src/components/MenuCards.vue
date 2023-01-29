a3d4902291649be10cddad11d6a6c2fd763fb01e
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 12:48:11 +0300

    Update MenuCards.vue

--- src/components/MenuCards.vue
+++ src/components/MenuCards.vue
@@ -1,21 +1,29 @@
 <template>
     <div class="row row-container">
+        <a :id="this.category.id"></a>
+        <h2 class="category-name">{{ this.category.name }}</h2>
         <MenuCard v-for="item in menu" :key="item.id" :menuItem="item" />
     </div>
 </template>
 
 <script>
-import menu from '../assets/menu.json';
-import MenuCard from '../components/MenuCard.vue'
+import * as menuService from '../services/menuService';
+import MenuCard from '../components/MenuCard.vue';
 export default {
     name: 'MenuCards',
     components: {
         MenuCard
     },
+    props: {
+        category: Object
+    },
     data() {
         return {
-            menu: menu
+            menu: []
         }
+    },
+    mounted() {
+        this.menu = menuService.getByCategory(this.category.id)
     }
 
 }
@@ -24,12 +32,19 @@ export default {
 <style>
 .row-container {
     float: left;
-    width: 80%;
+    width: 78%;
 }
 
 @media only screen and (min-width: 200px) and (max-width: 767px) {
     .row-container {
         width: 20%;
     }
+}
+
+.category-name {
+    text-align: left;
+    font-size: 32px;
+    font-weight: bolder;
+    margin-left: 10px;
 }
 </style>
