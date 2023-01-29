c5e42801ddec89e75c80b32e53b372d558faf4e3
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-01-29 12:48:09 +0300

    Update MenuCard.vue

--- src/components/MenuCard.vue
+++ src/components/MenuCard.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="col s12 m4 card-item-container">
+    <div class="col s12 m6">
 
 
         <div class="card ">
             <div class="card-image">
                 <img :key="menuItem.id" :src=menuItem.img>
             </div>
-            <div class="card-content ">
+            <div class="card-content card-item">
                 <span class="card-title" :key="menuItem.id">{{ menuItem.title }}</span>
                 <p class="truncate" :key="menuItem.id">
                     {{ menuItem.detail }}</p>
 
-                <MenuModal :menuItem="menuItem" :ingeridents="ingeridents" />
+                <MenuModal :menuItem="menuItem" />
 
                 <!-- <a class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons">+</i></a> -->
                 <!-- // v-model="showMenuModal" -->
@@ -23,7 +23,6 @@
 
 <script>
 import MenuModal from './MenuModal.vue';
-import { query } from '../services/ingeridentService';
 
 export default {
     name: 'MenuCard',
@@ -35,21 +34,21 @@ export default {
     },
     data() {
         return {
-            ingeridents: null,
         }
     },
-    mounted() {
-        this.ingeridents = query(this.menuItem);
-        console.log("ix", query(this.menuItem))
-    }
-
 }
 </script>
 
 <style  >
-.card-item-container {
-    /* display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap; */
+/* .menu-detail-text {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    white-space: pre-wrap;
 }
+
+.card-item {
+    overflow: hidden;
+} */
 </style>
